f737889ee8ee97b3c005458ea1167aba90b1e099
Martin Njogu <mndungu730@gmail.com>
2021-07-22 17:40:09 +0300

    Refactored code

--- src/main/java/com/pwebk/user/User.java
+++ src/main/java/com/pwebk/user/User.java
@@ -1,4 +1,20 @@
 package com.pwebk.user;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String email;
+    private String password;
 }
--- src/main/java/com/pwebk/user/UserController.java
+++ src/main/java/com/pwebk/user/UserController.java
@@ -1,4 +1,43 @@
 package com.pwebk.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    User findOne(@PathVariable int id){
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/")
+    User create(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    User update(@PathVariable int id, @RequestBody User user){
+        User olduser = userRepository.findById(id).orElse(null);
+        olduser.setName(user.getName());
+        olduser.setEmail(user.getEmail());
+        olduser.setPassword(user.getPassword());
+        return userRepository.save(olduser);
+    }
+
+    @GetMapping("/{id}")
+    Integer destroy(@PathVariable int id){
+        userRepository.deleteById(id);
+        return id;
+    }
 }
